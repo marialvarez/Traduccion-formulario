218d05018f88aa28829ad5425476ad503a681112
Maricela Alvarez <malvarezdrobush@gmail.com>
2017-01-13 10:08:06 -0500

    Finish

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.getElementById("form-signin-heading").innerHTML="Por favor inicia sesión";
+document.getElementById("inputEmail").placeholder="Correo electrónico";
+document.getElementById("inputPassword").placeholder="Contraseña";
+document.querySelector("span").innerHTML="Recordar datos";
+document.querySelector("button").innerHTML="Iniciar Sesión";
+
